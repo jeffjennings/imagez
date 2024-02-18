2f62f4fa39398408ddb997a31609a3a057b6335a
Jeff Jennings <jjennings1519@gmail.com>
2024-02-17 20:11:31 -0500

    slit_image: check initial frame

--- slit_image.py
+++ slit_image.py
@@ -10,3 +10,12 @@ nframe = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = int(vid.get(cv2.CAP_PROP_FPS))
 print(f"Video has {nframe} frames at {fps} FPS")
 
+# plot first frame
+_, frame0 = vid.read()
+# flip BGR to RGB
+frame0 = frame0[:,:,::-1]
+plt.figure()
+plt.imshow(frame0)
+plt.title('Frame 0')
+plt.show()
+
