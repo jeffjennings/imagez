1afcb56e73000bcfde70dee0f3ab94d52d3f0e9a
Jeff Jennings <jjennings1519@gmail.com>
2024-02-17 20:09:52 -0500

    remove package files

--- AUTHORS.rst
+++
@@ -1,4 +0,0 @@
-.. :authors:
-
-Authors:
-    - `Jeff Jennings (Penn State University) <https://github.com/jeffjennings>`_
--- setup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-from setuptools import setup
-
-# setup configuration is in `setup.cfg`
-setup()
